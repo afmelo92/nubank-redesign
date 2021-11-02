8831e6fd2b3dd99538ee3c786e6b1750e6f59e04
Andre <andre.fabian.melo@gmail.com>
2021-11-02 16:14:15 -0300

    Add menu styles with custom js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-unused-vars */
+// SCROLL HEADER
 function scrollHeader() {
   const nav = document.getElementById('header')
 
@@ -10,3 +11,47 @@ function scrollHeader() {
 }
 
 window.addEventListener('scroll', () => scrollHeader())
+
+// DROPDOWN MENU
+const dropdownMenu1 = document.getElementById('dropdown-item-1')
+const dropdownMenu2 = document.getElementById('dropdown-item-2')
+const dropdownMenu3 = document.getElementById('dropdown-item-3')
+
+const menuItem1 = document.getElementById('menu-item-1')
+const menuItem2 = document.getElementById('menu-item-2')
+const menuItem3 = document.getElementById('menu-item-3')
+
+function ShowDropdown1(event) {
+  event.preventDefault()
+  dropdownMenu1.classList.toggle('active-dropdown-menu')
+  dropdownMenu2.classList.remove('active-dropdown-menu')
+  dropdownMenu3.classList.remove('active-dropdown-menu')
+}
+
+function ShowDropdown2(event) {
+  event.preventDefault()
+  dropdownMenu2.classList.toggle('active-dropdown-menu')
+  dropdownMenu1.classList.remove('active-dropdown-menu')
+  dropdownMenu3.classList.remove('active-dropdown-menu')
+}
+
+function ShowDropdown3(event) {
+  event.preventDefault()
+  dropdownMenu3.classList.toggle('active-dropdown-menu')
+  dropdownMenu1.classList.remove('active-dropdown-menu')
+  dropdownMenu2.classList.remove('active-dropdown-menu')
+}
+
+function RemoveDropdown() {
+  dropdownMenu1.classList.remove('active-dropdown-menu')
+  dropdownMenu2.classList.remove('active-dropdown-menu')
+  dropdownMenu3.classList.remove('active-dropdown-menu')
+}
+
+menuItem1.addEventListener('click', ShowDropdown1)
+menuItem2.addEventListener('click', ShowDropdown2)
+menuItem3.addEventListener('click', ShowDropdown3)
+
+dropdownMenu1.addEventListener('mouseleave', RemoveDropdown)
+dropdownMenu2.addEventListener('mouseleave', RemoveDropdown)
+dropdownMenu3.addEventListener('mouseleave', RemoveDropdown)
